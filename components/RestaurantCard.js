d4463641b6a83060139c151a19f0776c0b415477
juancamilorivas <bogota.diamond@hotmail.com>
2022-08-11 19:20:51 -0500

    Every looks good and working, deliveryScreen finished and working

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -12,7 +12,7 @@ const RestaurantCard = ({
   imgUrl,
   title,
   rating,
-  genre,
+  genre,  
   address,
   short_description,
   dishes,
@@ -40,13 +40,15 @@ const RestaurantCard = ({
       })
     }}
 
-    className="bg-white mr-3 shadow">
+    className="bg-white mr-3 shadow"
+    >
       <Image
         source={{
           uri: urlFor(imgUrl).url(),
         }}
         className="h-36 w-64 rounded-sm"
       />
+
       <View className="px-3 pb-4">
         <Text className="font-bold text-lg pt-2">{title}</Text>
         <View className="flex-row items-center space-x-1">
--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,12 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { XIcon } from "react-native-heroicons/solid";
+import * as Progress from "react-native-progress";
+import MapView, { Marker } from "react-native-maps";
 
 const DeliveryScreen = () => {
-  return (
-    <View>
-      <Text>DeliveryScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
 
-export default DeliveryScreen
+  return (
+    <View className="bg-[#00CCBB] flex-1">
+      <SafeAreaView className="z-50">
+        <View className="flex-row justify-between items-center p-5">
+          <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+            <XIcon color="white" size={30} />
+          </TouchableOpacity>
+          <Text className="font-light text-white text-lg">Order Help</Text>
+        </View>
+
+        <View className="bg-white mx-5 my-2 rounded-md p-6 z-50 shadow-md">
+          <View className="flex-row justify-between">
+            <View>
+              <Text className="text-lg text-gray-400">Estimated Arrival</Text>
+              <Text className="text-4xl font-bold">45-55 Minutes</Text>
+            </View>
+            <Image
+              source={{ uri: "https://links.papareact.com/fls" }}
+              className="w-20 h-20"
+            />
+          </View>
+          <Progress.Bar size={30} color="#00CCBB" indeterminate={true} />
+
+          <Text className="mt-3 text-gray-500"></Text>
+        </View>
+      </SafeAreaView>
+
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.long,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        className="flex-1 -mt-10 z-0"
+        mapType="mutedStandard"
+      >
+        <Marker
+          coordinate={{ latitude: restaurant.lat, longitude: restaurant.long }}
+          title={restaurant.title}
+          description={restaurant.short_description}
+          identifier="origin"
+          pinColor="red"
+        />
+      </MapView>
+
+      <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28">
+        <Image
+          source={{
+            uri: "https://user-images.githubusercontent.com/67404147/173031349-5454733d-1332-43f9-9740-6c2e5064c701.png",
+          }}
+          className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+        />
+        <View className="flex-1">
+          <Text className="text-lg">Juan Camilo Rivas M.</Text>
+          <Text className="text-gray-400">Your Rider</Text>
+        </View>
+        <Text className="text-[#00CCBB] text-lg mr-5 font-bold">Call</Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default DeliveryScreen;
